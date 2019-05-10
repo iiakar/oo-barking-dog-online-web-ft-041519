4e7c5a4a8c85fe07e8a1f2a5cd1bd1b6366e1015
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-10 13:07:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
-Class Dog 
-end 
+class Dog
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def bark
+    puts "woof!"
+  end
 
-attr_accessor :name 
-attr_reader    :name 
 
-def bark 
-  puts "woof!"
-end 
+ end
